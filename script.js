6b7b2a7b888e61c03b3dfe92478fd6a42c04ffe3
salman <70580281+salman9011@users.noreply.github.com>
2023-11-17 22:55:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const makeChange = (c) => {
   // your name here
+	let units={
+		penny:0.01,
+		Nickle:0.05,
+		Dime:0.10,
+		Quater:0.25
+		}
+	let output={}
+	for(let i in units){
+		let currentUnits=units[i];
+		output[i]=parseInt(num/currentUnits);
+		num=num%currentUnits;
+	}
+	console.log(output);
+	
 };
 
 // Do not the change the code below
