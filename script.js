570285d941c901c0470ed2cea7cc3649370e9729
salman <70580281+salman9011@users.noreply.github.com>
2023-11-17 23:01:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
 const makeChange = (c) => {
-  // your name here
-	let units={
-		q:25,
-	   d:10,
-		n:5,
-		p:1
-		
-		}
-	let output={}
-	for(let i in units){
-		let currentUnits=units[i];
-		output[i]=parseInt(num/currentUnits);
-		num=num%currentUnits;
-	}
-	alert(output); 
-	
+  let num = parseInt(c); // Parse the input to ensure it's a number
+  let units = {
+    q: 25,
+    d: 10,
+    n: 5,
+    p: 1,
+  };
+  let output = {};
+  for (let i in units) {
+    let currentUnits = units[i];
+    output[i] = Math.floor(num / currentUnits); // Use Math.floor to get the whole number of coins
+    num = num % currentUnits;
+  }
+  return output; // Return the result instead of using alert
 };
 
-// Do not the change the code below
+// Do not change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
